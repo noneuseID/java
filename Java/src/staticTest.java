10f07c5538fe82c62de790defef274be27207aa8
kyr7070 <kyr7070@naver.com>
2019-05-29 22:25:54 +0900

    스태틱예제

---
+++ Java/src/staticTest.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class staticTest {
+    static Scanner in=new Scanner(System.in);
+    static boolean flag=true;
+    static int B,H;
+    static{
+        B=in.nextInt();
+        H=in.nextInt();
+        if(B<=0 || H<=0){
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+
+    }
+
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
